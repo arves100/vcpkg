df6447cf3beda2eb94727d6303175ad066ef71eb
Arves100 <arves100@gmail.com>
2020-06-02 19:16:34 +0200

    Crosstoolchain triplet for linux and freebsd

--- triplets/x86-freebsd.cmake
+++ triplets/x86-freebsd.cmake
@@ -3,3 +3,13 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME FreeBSD)
+
+if (NOT DEFINED ENV{VCPKG_ROOT})
+        message(FATAL_ERROR "
+        Please set an environment variable VCPKG_ROOT
+        For example:
+        export VCPKG_ROOT=/path/to/vcpkg
+        ")
+endif()
+
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/toolchains/freebsd32.cmake")
